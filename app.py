5f0f29930b78f927fbb215ac9f2f0fe539954644
Jérémy MARTIN <jeremy.delpech.martin@gmail.com>
2025-02-09 00:16:12 +0100

    feat: HelloWorld

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+
+
+
+def app():
+    st.write("Hello world!")
+
+    tab1, tab2, tab3, tab4 = st.tabs(['Cat', 'Dog', 'Owl', 'Bat'])
+
+    with tab1:
+        st.header('Chat:')
+        st.image('https://cdn.midjourney.com/91aefdc1-43b0-401a-ac4b-172799b0bfab/0_0.png', width=200)
+
+    with tab2:
+        st.header('Dog:')
+        st.image('https://cdn.midjourney.com/36e542d4-2c53-4234-be3c-2d0a7e5e0cb3/0_0.png', width=200)
+
+    with tab3:
+        st.header('Owl:')
+        st.image('https://cdn.midjourney.com/e60d2053-a3fb-4625-98e9-387016b5412b/0_1.png', width=200)
+
+    with tab4:
+        st.header('Bat:')
+        st.image('https://cdn.midjourney.com/bd145cc1-63e5-4e21-bfa2-e7c190f578b5/0_3.png', width=200)
+
+if __name__ == "__main__":
+    app()
